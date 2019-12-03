9ee67ccea8d4a84fe8b932f051b94bb3e0e4c0d9
Robert Vinluan <rvinluan@sidefx.com>
2019-12-03 09:42:13 -0500

    Leak the BindingManager singleton.
    This is to avoid an infrequent crash-on-exit in Houdini which we suspect
    is caused by our non-traditional integration of Python and Qt.  Our
    theory is that by the time the BindingManager static object is
    destroyed, Qt and Python have shutdown but Python did not destroy all
    objects still marked for GC.  Some of those objects are PySide2 objects
    which have dangling pointers to destroyed Qt objects.  Therefore, when
    the BindingManager attempts to clean up PySide2 references at
    destruction time it then produces a seg fault.

--- sources/shiboken6/libshiboken/bindingmanager.cpp
+++ sources/shiboken6/libshiboken/bindingmanager.cpp
@@ -177,8 +177,8 @@ BindingManager::~BindingManager()
 }
 
 BindingManager &BindingManager::instance() {
-    static BindingManager singleton;
-    return singleton;
+    static BindingManager *singleton = new BindingManager();
+    return *singleton;
 }
 
 bool BindingManager::hasWrapper(const void *cptr)
