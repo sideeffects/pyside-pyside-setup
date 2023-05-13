cee50b28fe0c0eb75099dc55f4a9a042d096b64c
Robert Vinluan <rvinluan@sidefx.com>
2023-05-13 08:54:55 -0400

    Remove reference to NPY_ARRAY_UPDATEIFCOPY.
    The symbol was deprecated and then removed in NumPy 1.23.

--- sources/shiboken2/libshiboken/sbknumpyarrayconverter.cpp
+++ sources/shiboken2/libshiboken/sbknumpyarrayconverter.cpp
@@ -116,8 +116,10 @@ std::ostream &operator<<(std::ostream &str, PyArrayObject *o)
             str << " NPY_ARRAY_NOTSWAPPED";
         if ((flags & NPY_ARRAY_WRITEABLE) != 0)
             str << " NPY_ARRAY_WRITEABLE";
+#if 0
         if ((flags & NPY_ARRAY_UPDATEIFCOPY) != 0)
             str << " NPY_ARRAY_UPDATEIFCOPY";
+#endif
     } else {
         str << '0';
     }
